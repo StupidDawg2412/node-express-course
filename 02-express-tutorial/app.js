1d4ce3ec57556dd1ae9a42d5a1ef51de9d2acb0c
seksan <koiiok5678@gmail.com>
2023-05-11 14:32:42 +0700

    clone from john smilga

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const port = 5000
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200,{'content-type': 'text/html'})
+    res.write('<h1>Home page</h1>')
+    res.end()
+})
+
+server.listen(port, ()=>{
+    console.log(`Server in listening on port : ${port}...`)
+})
