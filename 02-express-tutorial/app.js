409c6eec6b407a683459b25e04d44932674fb340
seksan <koiiok5678@gmail.com>
2023-05-11 18:57:12 +0700

    add new files

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,22 @@
 const express = require('express')
 const app = express()
-const {logger} = require('./logger.js')
-const {authorize} = require('./authorize.js')
+const {router} = require('./route/people')
 
-app.use([authorize, logger])
+app.use(express.static('methods-public'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use('/api/people',router)
 
-app.get('/', (req, res)=>{
-    res.send('Home')
-})
-app.get('/about', (req, res)=>{
-    res.send('About')
+app.post('/login', (req, res)=>{
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please enter name')
 })
 
-app.get('/api/products', (req, res)=>{
-    res.send('Products')
-})
-app.get('/api/items', (req, res)=>{
-    res.send('Items')
-})
-app.get('/api/users', (req, res)=>{
-    res.send('Users')
-})
+
+
 app.listen(5000, ()=>{
     console.log("Server is running on port : 5000...")
 })
---
+++ 02-express-tutorial/route/people.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const {people} = require('../data')
+const router = express.Router()
+
+
+router.get('/', (req,res)=>{
+    res.status(200).json({
+        success: true, data: people
+    })
+})
+
+router.post('/',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success: false, msg: 'please provide name value'})
+    }
+    res.status(201).json({success: true, person: name})
+})
+
+router.post('/postman', (req, res) => {
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success: false, msg: 'please provide name value'})
+    }
+    res.status(201).json({success: true, data:[...people, {"name":name, id: Math.random()*10 } ]})
+})
+
+router.put('/:id', (req, res)=>{
+    const {id} = req.params
+    const {name} = req.body
+
+    const person = people.find(person => person.id === Number(id) )
+
+    if(!person){
+        return res.status(404).json({success: false, msg: `No person with id : ${id}`})
+    }
+    const newPeople = people.map(person => {
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(201).json({success: true, data:[ newPeople ]})
+})
+
+router.delete('/:id', (req, res)=>{
+    const {id} = req.params
+    const person = people.find(person => person.id === Number(id) )  
+    if(!person){
+        return res.status(400).json({success: false, msg:`Cannot find a person with id : ${id}`})
+    }
+    const newPeople = people.filter(person=> person.id !== Number(id) )
+    res.status(201).json({success: true, data:[ newPeople ]})
+})
+
+module.exports = {router}
