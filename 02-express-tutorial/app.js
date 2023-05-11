7d2364955b2eaf7917b733d7541d928b31da2bcb
seksan <koiiok5678@gmail.com>
2023-05-11 15:06:47 +0700

    install express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,2 @@
-const http = require('http')
+const express = require('express')
 
-const port = 5000
-
-const server = http.createServer((req, res)=>{
-    res.writeHead(200,{'content-type': 'text/html'})
-    res.write('<h1>Home page</h1>')
-    res.end()
-})
-
-server.listen(port, ()=>{
-    console.log(`Server in listening on port : ${port}...`)
-})
