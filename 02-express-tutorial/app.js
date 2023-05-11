396f65efe2f6483bf5fc7a85b33044bceba4465a
seksan <koiiok5678@gmail.com>
2023-05-11 16:46:30 +0700

    add logger middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,2 +1,27 @@
 const express = require('express')
+const app = express()
+const {logger} = require('./logger.js')
+
+app.use(logger)
+
+app.get('/', (req, res)=>{
+    res.send('Home')
+})
+app.get('/about', (req, res)=>{
+    res.send('About')
+})
+
+app.get('/api/products', (req, res)=>{
+    res.send('Products')
+})
+app.get('/api/items', (req, res)=>{
+    res.send('Items')
+})
+app.get('/api/users', (req, res)=>{
+    res.send('Users')
+})
+app.listen(5000, ()=>{
+    console.log("Server is running on port : 5000...")
+})
+
 
