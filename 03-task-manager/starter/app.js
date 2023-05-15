836ff10bd57ea6e76b72634bed874edea8c339e2
Strong_N_Stupid <63193755+StupidDawg2412@users.noreply.github.com>
2023-05-15 11:37:15 +0700

    starting up 03 task manager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const http = require('http')
+const port = 5000
+const server = http.createServer((req,res)=>{
+    console.log(req.url)
+    res.write('<h1>Test</h1>')
+})
+
+server.listen(5000, ()=>{
+    console.log(`Server is running on port : ${port}`)
+})
